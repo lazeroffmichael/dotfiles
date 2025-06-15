36c13a46512d7592d1b99239ac5a683bf3fd38f0
Michael Lazeroff <56806733+lazeroffmichael@users.noreply.github.com>
2025-06-15 11:09:03 -0700

    add tpm comment stowinstall

--- stowInstall.sh
+++ stowInstall.sh
@@ -7,6 +7,7 @@ do
     stow --dotfiles $folder
 done
 
+# Install tpm
 if [ ! -d "$HOME/.config/tmux/plugins/tpm" ];  then
     echo "TMUX plugin directory does not exist"
     git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
